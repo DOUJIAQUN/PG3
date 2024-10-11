e6a1b713d2cf82e3fc04f550361b81b30f54d67e
LC1C_22_トウ_カグン <146914486+DOUJIAQUN@users.noreply.github.com>
2024-10-11 11:24:34 +0900

    再帰関数

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
-#include<stdio.h>
+
+#include <stdio.h>
+
+int GeneralWages(int n) {
+	int wages = 1072;
+	return(wages * n);
+}
+
+int RecursiveWages(int n) {
+
+	if (n == 1) {
+		return(100);
+	}
+
+	return (RecursiveWages(n - 1) * 2 - 50);
+}
+
+void CompareWages(int n) {
+	int rResult, gResult;
+	rResult = RecursiveWages(n);
+	gResult = GeneralWages(n);
+
+	printf("%d時間働いた場合\n一般的な資金体系 : %d\n再帰的な資金体系 : %d\n\n", n, gResult, rResult);
+}
 
 int main() {
+	int n = 12;
+	for (int i = 1; i <= n; i++) {
+		CompareWages(i);
+	}
 
-	printf("こんにちは");
+
 
 	return 0;
 }
