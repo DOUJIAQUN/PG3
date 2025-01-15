77a27fc064b1306f6204f2b3056ee6a52f762ff9
LC1C_22_トウ_カグン <146914486+DOUJIAQUN@users.noreply.github.com>
2025-01-15 14:33:17 +0900

    07_01完了

--- main.cpp
+++ main.cpp
@@ -1,23 +1,27 @@
-﻿
-#include <stdio.h>
-#include <thread>
+﻿#include <stdio.h>
+#include <string>
+#include <chrono>
 
-void one() {
-	printf("shread1\n");
-}
-void two() {
-	printf("thread2\n");
-}
-void three() {
-	printf("thread3\n");
-}
-int main()
-{
-	std::thread th1(one);
-	th1.join();
-	std::thread th2(two);
-	th2.join();
-	std::thread th3(three);
-	th3.join();
+int main() {
+    std::string a(100000, 'a');
 
+    // コピー時間の計測
+    auto start_copy = std::chrono::high_resolution_clock::now();
+    std::string b = a;
+    auto end_copy = std::chrono::high_resolution_clock::now();
+
+    auto copy_duration = std::chrono::duration_cast<std::chrono::microseconds>(end_copy - start_copy);
+
+    // 移動時間の計測
+    auto start_move = std::chrono::high_resolution_clock::now();
+    std::string c = std::move(a);
+    auto end_move = std::chrono::high_resolution_clock::now();
+
+    auto move_duration = std::chrono::duration_cast<std::chrono::microseconds>(end_move - start_move);
+
+    printf("100,000文字を移動とコピーで比較しました。\n");
+    printf("コピー: %lld μs\n", copy_duration.count());
+    printf("移動: %lld μs\n", move_duration.count());
+
+    return 0;
 }
